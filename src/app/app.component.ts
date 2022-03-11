17a3b802a49640cc86c35dd5a70546a4b40e39d2
Priyanka-102001 <priyankadhangat@gmail.com>
2022-03-11 15:44:29 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  
+}
+@Component({
+  selector: 'app-click-me',
+  template: `
+    <button type="button" (click)="onClickMe()">Click me!</button>
+    {{clickMessage}}`
+})
+export class ClickMeComponent {
+  clickMessage = '';
+
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+  }
+}
